008bd3b2d4c8287194db18d4678b421ceace6cf8
Khaled Amr <56801590+khaledamrayyad@users.noreply.github.com>
2020-02-02 04:04:11 +0200

    Add files via upload

---
+++ Simple Chatty Bot/Problems/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,17 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1=scanner.next();
+        String s2=scanner.next();
+        String num1=scanner.next();
+
+        System.out.println(num1);
+        System.out.println(s2);
+        System.out.println(s1);
+
+    }
+}
---
+++ Simple Chatty Bot/Problems/Printing strings in the same order/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1=scanner.next();
+        String s2=scanner.next();
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
